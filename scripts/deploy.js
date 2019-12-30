74e8212c5a8e7d11636ac7d30fb64aa8a5df05e2
fugroup <vidar@fugroup.net>
2019-12-30 01:45:47 +0100

    Add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,9 @@
-console.log('Not implemented yet...')
+const sh = require('shelljs')
+const fspath = require('path')
+const from = process.argv[3]
+const to = process.argv[4]
+if (from && to) {
+  sh.exec(`scp -rp ${from} ${to}`)
+} else {
+  console.log('Usage: waveorb deploy [from] [to]')
+}
