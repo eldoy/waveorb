7bf4435e86f2dbb6e93ffd11b4067d71b1bc5669
Vidar Eldøy <vidar@eldoy.com>
2020-01-19 19:17:10 +0100

    Extra space in log

--- scripts/create.js
+++ scripts/create.js
@@ -5,7 +5,7 @@ const name = process.argv[3] || 'default'
 const path = fspath.join(__dirname, '..', 'templates', name, 'app')
 if (fs.existsSync(path)) {
   sh.cp('-R', path, '.')
-  console.log(`App ${name} copied to 'app' directory`)
+  console.log(`\nApp ${name} copied to 'app' directory`)
 } else {
   console.log(`\nTemplate ${name} does not exist.`)
 }
