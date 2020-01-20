8a2940dc55ccda3ab3b1eeee244391d66df56501
Vidar Eldøy <vidar@eldoy.com>
2020-01-20 15:02:34 +0100

    Fetch template from github

--- scripts/create.js
+++ scripts/create.js
@@ -1,11 +1,38 @@
-const sh = require('shelljs')
+const appName = process.argv[3]
+if (!appName) {
+  console.log(`\nUsage: waveorb create [name]`)
+  console.log(`\nPlease specify a directory name.`)
+  process.exit(1)
+}
+
 const fs = require('fs')
+if (fs.existsSync(appName)) {
+  console.log(`\nThe ${appName} directory already exists.`)
+  console.log(`\nPlease delete it and try again.`)
+  process.exit(1)
+}
+
+const os = require('os')
 const fspath = require('path')
-const name = process.argv[3] || 'default'
-const path = fspath.join(__dirname, '..', 'templates', name, 'app')
+const sh = require('shelljs')
+const repo = 'https://github.com/eldoy/waveorb-templates.git'
+const dir = Math.random().toString(36).slice(2)
+const tmp = fspath.join(os.tmpdir(), dir)
+sh.exec(`git clone --depth 1 ${repo} ${tmp}`)
+
+const templateName = process.argv[4] || 'default'
+const path = fspath.join(tmp, templateName)
 if (fs.existsSync(path)) {
-  sh.cp('-R', path, '.')
-  console.log(`\nApp ${name} copied to 'app' directory`)
+  sh.cp('-R', path, appName)
+  console.log([
+    `\nApp ${templateName} copied to ${appName}, now do:\n`,
+    `cd ${appName}`,
+    `npm i`,
+    `waveorb serve\n`,
+    `Docs: https://waveorb.com/docs`,
+    `Issues: https://github.com/eldoy/waveorb/issues\n`,
+    `Created by Eldøy Projects, https://eldoy.com`
+  ].join('\n'))
 } else {
-  console.log(`\nTemplate ${name} does not exist.`)
+  console.log(`\nTemplate ${templateName} does not exist.`)
 }
--- templates/default/app/pages/about.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function($) {
-  $.page.title = 'About'
-
-  return /* html */`
-    <h1>about</h1>
-    <p>it is ultra light weight and extremely easy to use. ready to code?</p>
-  `
-}
