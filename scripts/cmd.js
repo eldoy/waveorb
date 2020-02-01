27b65f4ecf10f3cf51027f2bbc56d943ced64482
Vidar Eldøy <vidar@eldoy.com>
2020-02-01 04:31:02 +0100

    Update waveorb client

--- scripts/cmd.js
+++ scripts/cmd.js
@@ -8,32 +8,32 @@ const server = process.argv[3] || 'http://localhost:5000'
 const client = waveorb(server)
 
 const api = {}
+api.action = async function(name, params, options) {
+  try {
+    return await client.action(name, params, options)
+  } catch (e) {
+    console.log(`${e.name}: ${e.message}`)
+  }
+}
+
+api.upload = async function(name, params, options) {
+  try {
+    return await client.upload(name, params, options)
+  } catch (e) {
+    console.log(`${e.name}: ${e.message}`)
+  }
+}
+
 api.help = function() {
   console.log([
     `\nWaveorb cmd v${package.version}\n`,
     `Usage: waveorb cmd [server]\n`,
-    `Example: await fetch({ action: 'name' })\n`,
+    `Example: await action('name')\n`,
     `Docs: https://waveorb.com/doc/command-line.html#cmd\n`,
     'Available functions:\n',
     Object.keys(api).map(x => `  ${x}()`).join('\n'),
     `\nConnected to ${server}\n`
   ].join('\n'))
-}
-
-api.fetch = async function(params, options) {
-  try {
-    return await client.fetch(params, options)
-  } catch (e) {
-    console.log(`${e.name}: ${e.message}`)
-  }
-}
-
-api.upload = async function(params, options) {
-  try {
-    return await client.upload(params, options)
-  } catch (e) {
-    console.log(`${e.name}: ${e.message}`)
-  }
 }
 
 api.help()
