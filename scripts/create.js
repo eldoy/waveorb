45ba4e2de960972f92ea5da0ae5ff93cec4d89a8
Vidar Eldøy <vidar@eldoy.com>
2021-02-13 17:07:07 +0100

    Update packages

--- scripts/create.js
+++ scripts/create.js
@@ -1,29 +1,33 @@
+const path = require('path')
+const fs = require('fs')
+const crypto = require('crypto')
+const os = require('os')
+const sh = require('shelljs')
+
 const appName = process.argv[3]
+
 if (!appName) {
   console.log(`\nUsage: waveorb create [name]`)
   console.log(`\nPlease specify a directory name.`)
   process.exit(1)
 }
 
-const fs = require('fs')
 if (fs.existsSync(appName)) {
   console.log(`\nThe ${appName} directory already exists.`)
   console.log(`\nPlease delete it and try again.`)
   process.exit(1)
 }
-const crypto = require('crypto')
-const os = require('os')
-const fspath = require('path')
-const sh = require('shelljs')
+
 const repo = 'https://github.com/eldoy/waveorb-templates.git'
 const dir = crypto.randomBytes(20).toString('hex')
-const tmp = fspath.join(os.tmpdir(), dir)
+const tmp = path.join(os.tmpdir(), dir)
 sh.exec(`git clone --depth 1 ${repo} ${tmp}`)
 
 const templateName = process.argv[4] || 'default'
-const path = fspath.join(tmp, templateName)
-if (fs.existsSync(path)) {
-  sh.cp('-R', path, appName)
+const dir = path.join(tmp, templateName)
+
+if (fs.existsSync(dir)) {
+  sh.cp('-R', dir, appName)
   console.log([
     `\nApp ${templateName} copied to ${appName}, now do:\n`,
     `cd ${appName}`,
