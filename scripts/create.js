b7155f7ac769ca07dcaa6d13f8683f4ee92c1874
Vidar Eldøy <vidar@eldoy.com>
2021-02-14 05:09:50 +0100

    Silent npm

--- scripts/create.js
+++ scripts/create.js
@@ -38,7 +38,7 @@ if (!extras.exist(dir)) {
 }
 
 extras.copy(path.join(dir, '*'), '.')
-extras.run('npm install')
+extras.run('npm --quiet install')
 
 console.log(`\nWaveorb app created, now do:\n`)
 t.green(`cd ${name}\nnpm run serve\n\n`)
