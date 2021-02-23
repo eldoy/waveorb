a06ad814111dd9becc90e5b285d0e8a44971e013
Vidar Eldøy <vidar@eldoy.com>
2021-02-23 01:45:18 +0100

    Update build script

--- scripts/build.js
+++ scripts/build.js
@@ -82,9 +82,9 @@ async function build() {
     }
   }
   console.log(`Build complete...\n`)
-  await sleep(2)
+  await sleep(1)
   console.log(`Shutting down build server...\n`)
-  server.http.server.close()
+  server.http.close()
   await sleep(1)
 
   // Build assets
