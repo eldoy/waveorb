4600a7f72ff6b83cdb865f7189fa3661d4be1736
Vidar Eldøy <vidar@eldoy.com>
2021-02-23 22:19:28 +0100

    No need for the repo

--- scripts/update.js
+++ scripts/update.js
@@ -12,4 +12,4 @@ const domain = (config.domains || config.domains?.[0]?.names || '').split(' ')[0
 if (!domain) exit(`No valid domain name was found!`)
 
 // ssh into domain and run update.js
-run(`ssh root@${domain} 'cd waveorb-server && node update.js ${repo}'`)
+run(`ssh root@${domain} 'cd waveorb-server && node update.js'`)
