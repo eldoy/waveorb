7bbe958d374af88d31827795f87f22d011ab66e6
Vidar Eldøy <vidar@eldoy.com>
2021-02-25 02:29:46 +0100

    Lookup ip on deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -19,5 +19,10 @@ try {
 const domain = (config.domains || config.domains?.[0]?.names || '').split(' ')[0]
 if (!domain) exit(`No valid domain name was found!`)
 
-// ssh into domain and run deploy.js
-run(`ssh root@${domain} 'cd waveorb-server && node deploy.js ${repo}'`)
+dns.lookup(domain, (err, ip) => {
+  if(err) {
+    exit(`The domain ${domain} does not have an ip address!`)
+  }
+  // ssh into domain and run deploy.js
+  run(`ssh root@${ip} 'cd waveorb-server && node deploy.js ${repo}'`)
+})
