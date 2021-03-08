8f37ba0113910da34efd0f952308a79d81a863d2
Vidar Eldøy <vidar@eldoy.com>
2021-03-08 16:34:35 +0100

    Get sitemap from config

--- scripts/ping.js
+++ scripts/ping.js
@@ -1,17 +1,28 @@
+const got = require('got')
+const { exist, read } = require('extras')
 let sitemap = process.argv[3]
+if (exist('waveorb.json')) {
+  const config = tools.read('waveorb.json')
+  if (typeof config.sitemap == 'string') {
+    sitemap = config.sitemap
+  } else if (typeof config.domains == 'string') {
+    const domain = config.domains.split(' ')[0]
+    sitemap = `https://${domain}/sitemap.xml`
+  }
+}
+
 if (!sitemap) {
   console.log(`\nUsage: waveorb ping [sitemap url]`)
   console.log(`\nExample: waveorb ping https://example.com/sitemap.xml`)
   process.exit(1)
 }
+console.log(`Submitting sitemap ${sitemap}...`)
 sitemap = encodeURIComponent(sitemap)
-const taarn = require('taarn')
+
 const bing = `http://bing.com/ping?sitemap=${sitemap}`
 const google = `http://www.google.com/ping?sitemap=${sitemap}`
 
 ;(async function() {
-  console.log('Submitting to', bing)
-  await taarn(bing, {}, { method: 'GET' })
-  console.log('Submitting to', google)
-  await taarn(google, {}, { method: 'POST' })
+  got.get(bing)
+  got.post(google)
 })()
