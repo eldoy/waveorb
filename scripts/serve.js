91ede3bf98ab4ed4a16e3dd7b5fb9ccfbdbc4e0a
Vidar Eldøy <vidar@eldoy.com>
2021-05-14 04:38:30 +0200

    Allow serve with package.json

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 const path = require('path')
 const root = process.cwd()
-const pkg = require(path.join(root, 'package.json'))
+let pkg
+try {
+  pkg = require(path.join(root, 'package.json'))
+} catch (e) {}
 if (pkg && pkg.main) {
   require(path.join(root, pkg.main))
 } else {
