2ac2ff4ca465a63823c7491900aec75419e16a63
Vidar Eldøy <vidar@eldoy.com>
2021-09-14 04:56:27 +0200

    Move markup tests into pager

--- test/pager.test.js
+++ test/pager.test.js
@@ -1,50 +1,207 @@
 const pager = require('../lib/pager.js')
 const loader = require('../lib/loader.js')
 
+const req = { pathname: '/', query: {} }
+const res = {
+  setHeader: function() {}
+}
+
+const about = async function($) {
+  $.page.title = 'About'
+  return `<div>About</div>`
+}
+
+const t = function(key) {
+  return key
+}
+
+let $, app
+
+function flat(result) {
+  return (result || '').split('\n').map(x => x.trim()).join('')
+}
+
 describe('pager', () => {
-  afterEach(() => {
-    delete process.env.WAVEORB_APP
+  beforeAll(async () => {
+    process.env.WAVEORB_APP = 'test/apps/app22'
+    app = await loader()
   })
 
-  it('should find a page without routemap', async () => {
-    process.env.WAVEORB_APP = 'test/apps/app18'
-    const app = await loader()
-    const $ = { app }
+  beforeEach(() => {
+    $ = { app, req, res, t, page: {} }
+    req.pathname = '/'
+  })
 
-    let page = pager('about', $)
-    expect(page).toBeDefined()
+  it('should load the home page', async () => {
+    const page = await pager('index', $)
+    expect(await page($)).toBe(`<div>Home</div>`)
+  })
 
-    page = pager('index', $)
-    expect(page).toBeDefined()
+  it('should load the about page', async () => {
+    const page = await pager('about', $)
+    expect(await page($)).toBe(`<div>About</div>`)
+  })
 
-    page = pager('notfound', $)
-    expect(page).toBeUndefined()
-
-    page = pager('no/index', $)
-    expect(page).toBeUndefined()
-
-    page = pager('no/om-oss', $)
+  it('should not load the about page when path is wrong', async () => {
+    const page = await pager('something/about', $)
     expect(page).toBeUndefined()
   })
 
-  it('should find a page with routemap', async () => {
-    process.env.WAVEORB_APP = 'test/apps/app17'
-    const app = await loader()
-    const $ = { app }
+  it('should load the deep page', async () => {
+    const page = await pager('docs/deep', $)
+    expect(await page($)).toBe(`<div>Deep</div>`)
+  })
 
-    let page = pager('about', $)
-    expect(page).toBeDefined()
+  it('should load pages via routemap option as string', async () => {
+    $.app.config = {
+      routes: {
+        routemap: { '/om-oss.html': 'en@about' }
+      }
+    }
+    const page = await pager('om-oss', $)
+    expect(await page($)).toBe(`<div>About</div>`)
+  })
 
-    page = pager('index', $)
-    expect(page).toBeDefined()
+  it('should load pages via routemap option as string deeply', async () => {
+     $.app.config = {
+      routes: {
+        routemap: { '/hello/om-oss.html': 'en@about' }
+      }
+    }
+    const page = await pager('hello/om-oss', $)
+    expect(await page($)).toBe(`<div>About</div>`)
+  })
 
-    page = pager('notfound', $)
-    expect(page).toBeUndefined()
+  it('should work with dynamic routes', async () => {
+    const _index = async function($) {
+      return `<div>HTML</div>`
+    }
+    $.app = {
+      pages: {
+        _index
+      }
+    }
+    const page = await pager('dynamic', $)
+    expect(await page($)).toBe(`<div>HTML</div>`)
+  })
 
-    page = pager('no/index', $)
-    expect(page).toBeDefined()
+  it('should work with nested dynamic index routes', async () => {
+    const _index = async function($) {
+      return `<div>${$.req.query.index}</div>`
+    }
+    $.app = {
+      pages: {
+        doc: {
+          _index
+        }
+      }
+    }
+    const page = await pager('doc/hello', $)
+    expect(await page($)).toBe(`<div>hello</div>`)
+  })
 
-    page = pager('no/om-oss', $)
-    expect(page).toBeDefined()
+  it('should work with nested dynamic non index routes', async () => {
+    const _actions = async function($) {
+      return `<div>${$.req.query.actions}</div>`
+    }
+    $.app = {
+      pages: {
+        doc: {
+          _actions
+        }
+      }
+    }
+    const page = await pager('doc/hello', $)
+    expect(await page($)).toBe(`<div>hello</div>`)
+  })
+
+  it('should collect query params from URL', async () => {
+    const article = async function($) {
+      return `<div>${$.req.query.year}/${$.req.query.month}</div>`
+    }
+    $.app = {
+      pages: {
+        _year: {
+          _month: {
+            article
+          }
+        }
+      }
+    }
+    const page = await pager('2020/12/article', $)
+    expect(await page($)).toBe(`<div>2020/12</div>`)
+  })
+
+  it('should avoid dynamic route if template exists', async () => {
+    const _index = async function($) {
+      return `<div>HTML</div>`
+    }
+    $.app = {
+      pages: {
+        about,
+        _index
+      }
+    }
+    const page = await pager('about', $)
+    expect(await page($)).toBe(`<div>About</div>`)
+  })
+
+  it('should avoid dynamic route if template exists, sorted', async () => {
+    const _index = async function($) {
+      return `<div>HTML</div>`
+    }
+    $.app = {
+      pages: {
+        _index,
+        about
+      }
+    }
+    const page = await pager('about', $)
+    expect(await page($)).toBe(`<div>About</div>`)
+  })
+
+  it('should work with dynamic route and routemap option', async () => {
+    const _index = async function($) {
+      return `<div>HTML</div>`
+    }
+    $.app = {
+      pages: {
+        about,
+        _index
+      },
+      config: {
+        routes: {
+          routemap: {
+            '/something.html': 'en@something'
+          }
+        }
+      }
+    }
+    const page = await pager('om-oss', $)
+    expect(await page($)).toBe(`<div>HTML</div>`)
+  })
+
+  it('should collect query params from URL with routemap option', async () => {
+    const article = async function($) {
+      return `<div>${$.req.query.year}/${$.req.query.month}</div>`
+    }
+    $.app = {
+      pages: {
+        _year: {
+          _month: {
+            article
+          }
+        }
+      },
+      config: {
+        routes: {
+          routemap: {
+            '/_year/_month/artikkel.html': 'en@_year/_month/article'
+          }
+        }
+      }
+    }
+    const page = await pager('2020/12/artikkel', $)
+    expect(await page($)).toBe(`<div>2020/12</div>`)
   })
 })
