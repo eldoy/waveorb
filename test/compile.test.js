4c4bbd5b68a543375ca41f8608e02f071820b642
Vidar Eldøy <vidar@eldoy.com>
2022-09-20 17:04:59 +0200

    Re-introduce compile

---
+++ test/compile.test.js
@@ -0,0 +1,23 @@
+const compile = require('../lib/compile.js')
+
+describe('compile', () => {
+  it('should not compile templates if no matches', async () => {
+    const $ = { page: { content: 'hello' } }
+    compile($)
+    expect($.page.content).toBe('hello')
+  })
+
+  it('should compile translations', async () => {
+    const $ = { page: { content: "hello ${$.t('bye')}" } }
+    $.t = () => 'word'
+    compile($)
+    expect($.page.content).toBe("hello ${'word'}")
+  })
+
+  it('should compile links', async () => {
+    const $ = { page: { content: "hello ${$.link('bye')}" } }
+    $.link = () => 'word'
+    compile($)
+    expect($.page.content).toBe("hello ${'word'}")
+  })
+})
