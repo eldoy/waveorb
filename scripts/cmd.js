7fbb6d7d9cebaa7e859a39f8ded942e8b026ada0
Vidar Eldøy <vidar@eldoy.com>
2023-02-05 21:24:17 +0100

    Cmd hook, include db

--- scripts/cmd.js
+++ scripts/cmd.js
@@ -15,8 +15,26 @@ console.log(`
 `)
 
 async function start() {
+  // Indicate that we are running a cmd session
+  process.env.WAVEORB_CMD = 1
+
   const cmd = repl.start('ᚠ ')
-  api.app = await loader()
+  const app = await loader()
+
+  let objects
+  if (typeof app.hooks.cmd == 'function') {
+    objects = await app.hooks.cmd(app)
+  }
+
+  // Load db if it exists
+  const db = app.plugins?.db
+
+  // Load these objects into the cmd context
+  const api = {
+    app,
+    db,
+    ...objects
+  }
   Object.assign(cmd.context, api)
 }
 start()
