7a6c0eb3a7b6ed68612006076e27d99bb361105c
Vidar Eldøy <vidar@eldoy.com>
2023-05-13 07:51:06 +0200

    Address override option for deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,24 @@ const dns = require('dns')
 const { run, exit, get } = require('extras')
 const config = require('../lib/config.js')
 
+const env = process.argv[3]
+
 // Find git repo address
-let repo = config?.git || get(`git config --get remote.origin.url`)
+const repo = config?.git || get(`git config --get remote.origin.url`)
 if (!repo) exit('Git repository URL not found!')
+
+// ssh into domain and run deploy.js
+function deploy(address) {
+  const mode = env ? `WAVEORB_DEPLOY_ENV=${env} ` : ''
+  run(
+    `ssh root@${address} 'cd waveorb-server && ${mode}node deploy.js ${repo}'`
+  )
+}
+
+// Use specified address if defined
+if (config.address) {
+  return deploy(config.address)
+}
 
 // Find main domain name
 let domain = config.domains
@@ -18,13 +33,9 @@ domain = domain.split(' ')[0]
 
 if (!domain) exit(`No valid domain name was found!`)
 
-const env = process.argv[3]
-
-dns.lookup(domain, (err, ip) => {
+dns.lookup(domain, (err, address) => {
   if (err) {
     exit(`The domain ${domain} does not have an ip address!`)
   }
-  // ssh into domain and run deploy.js
-  const mode = env ? `WAVEORB_DEPLOY_ENV=${env} ` : ''
-  run(`ssh root@${ip} 'cd waveorb-server && ${mode}node deploy.js ${repo}'`)
+  deploy(address)
 })
