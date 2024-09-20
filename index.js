5fade7a1241b10f8cf6940e882e95f7790b8609a
Vidar Eldøy <vidar@eldoy.com>
2024-09-20 04:04:01 +0200

    fix: move globals to file

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var haka = require('haka')
-global.esc = haka.esc
-global.raw = haka.raw
-global.num = haka.num
-global.time = haka.time
-
 global.Waveorb = require('./lib/tools.js')
 
 module.exports = global.Waveorb
