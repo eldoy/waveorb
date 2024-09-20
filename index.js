ed87dcf25648082b0a824b510c693cc387269620
Vidar Eldøy <vidar@eldoy.com>
2024-09-20 03:59:41 +0200

    feature: add global functions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var haka = require('haka')
+global.esc = haka.esc
+global.raw = haka.raw
+global.num = haka.num
+global.time = haka.time
+
 global.Waveorb = require('./lib/tools.js')
 
 module.exports = global.Waveorb
